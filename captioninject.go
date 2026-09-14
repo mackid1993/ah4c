c507b1aa52b9bbd05960f0c392bddf7b6f6ef32a
mackid1993 <david@brustein.net>
2026-09-14 04:13:40 -0400

    Revert "Reset caption rate sampling across fragmented PES headers"
    This reverts commit 06a1ec693e36e2ed166c7c65a4cc972602f794c9.

--- captioninject.go
+++ captioninject.go
@@ -448,8 +448,6 @@ func (ci *captionInjector) packet(p []byte) error {
 				if _, _, pts, ok := splitPES(tsPayload(p)); ok {
 					ci.trackFrameRate(pts)
 					ci.onPicture(pts)
-				} else {
-					ci.ptsGaps = ci.ptsGaps[:0]
 				}
 			}
 		}
